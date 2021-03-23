5be65d8abac0825d568498d1961cc75434698855
Felix Swinkes <tgifelix@icloud.com>
2021-03-23 16:08:29 +0100

    more emojis :neckbeard:

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
 	export let bgColor;
+	let emoji = ['🤙','🤟', '😎', '🎩', '👨‍💻', '🚀'];
+	let favicon = emoji[Math.floor(Math.random()*emoji.length)];
 </script>
+
+<svelte:head>
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>{favicon}</text></svg>">
+</svelte:head>
 
 <main>
 	<div class='block'>
