718786319b7b0b8412731da72e68415382da4c20
Felix Swinkes <tgifelix@icloud.com>
2021-03-23 14:29:30 +0100

    #E79C85

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import 'https://unpkg.com/phosphor-icons';
 const app = new App({
 	target: document.body,
 	props: {
-		bgColor: '#FFEEB0',
+		bgColor: '#E79C85',
 	}
 });
 
 document.documentElement.style
-	.setProperty('--bg-color', '#FFEEB0');
+	.setProperty('--bg-color', '#E79C85');
 
 export default app;
